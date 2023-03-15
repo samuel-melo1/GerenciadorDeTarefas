ba1ec7085b3cd3e765a7a467a04f45283d6a2886
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-03-15 00:11:10 -0300

    inicio do projeto

---
+++ src/main/java/com/Tarefas/Gerenciador/GerenciadorApplication.java
@@ -0,0 +1,13 @@
+package com.Tarefas.Gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tarefas/Gerenciador/GerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarefas.Gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
