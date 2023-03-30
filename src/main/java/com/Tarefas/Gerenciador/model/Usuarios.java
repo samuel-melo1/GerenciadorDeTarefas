acf6d382ee6538b35bcbc6bfa02620439373b84b
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-03-29 23:09:18 -0300

    tela de login criada

--- src/main/java/com/Tarefas/Gerenciador/model/Usuarios.java
+++ src/main/java/com/Tarefas/Gerenciador/model/Usuarios.java
@@ -1,11 +1,9 @@
 package com.Tarefas.Gerenciador.model;
 
 
-import java.io.Serializable;
+
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
